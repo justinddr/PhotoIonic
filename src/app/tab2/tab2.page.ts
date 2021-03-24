5857670dab2f4369971bd2150081a2e6af39a6c9
Dadar <justindadar@yahoo.fr>
2021-03-24 16:37:46 +0100

    photo download 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,9 +9,12 @@ import { PhotoService } from '../services/photo.service';
 export class Tab2Page {
 
   constructor(public photoService: PhotoService) { }
-
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 addPhotoToGallery() {
   this.photoService.addNewToGallery();
+  console.log("test");
 }
 
 }
